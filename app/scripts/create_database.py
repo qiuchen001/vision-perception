dd1eca9db0fde13b5195ff2eabc92889fe14e273
zhangchenchen <zhangchenchen@51aes.com>
2025-03-07 14:48:11 +0800

    格式化

--- app/scripts/create_database.py
+++ app/scripts/create_database.py
@@ -3,9 +3,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+MILVUS_HOST = os.getenv("MILVUS_HOST")
+MILVUS_PORT = os.getenv("MILVUS_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-conn = connections.connect(host=os.getenv("SERVER_HOST"), port=19530)
-database = db.create_database("video_db")
-
-db.using_database("video_db")
+conn = connections.connect(host=MILVUS_HOST, port=MILVUS_PORT)
+database = db.create_database(DB_NAME)
+db.using_database(DB_NAME)
 print(db.list_database())
