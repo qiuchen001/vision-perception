3dabf57c878ddb7c11fd51db20d464de0f753508
zhangchenchen <zhangchenchen@51aes.com>
2025-03-11 13:40:39 +0800

    调整config

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
-app = create_app('config.Config')
+app = create_app('config.config.Config')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=30501, debug=True)
